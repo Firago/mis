d7d370e0856f7021b1565d0ffcf151081950c8e4
Firago <firago94@gmail.com>
2016-06-07 20:04:59 +0200

    Add: datetime picker instead of datepicker for lesson start field Add: allow CORS

--- src/main/webapp/scripts/app/entities/lesson/lesson-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/lesson/lesson-dialog.controller.js
@@ -2,47 +2,39 @@
 
 angular.module('misApp').controller('LessonDialogController',
     ['$scope', '$stateParams', '$uibModalInstance', 'entity', 'Lesson', 'SubjectEntry', 'Teacher', 'StudentGroup',
-        function($scope, $stateParams, $uibModalInstance, entity, Lesson, SubjectEntry, Teacher, StudentGroup) {
+        function ($scope, $stateParams, $uibModalInstance, entity, Lesson, SubjectEntry, Teacher, StudentGroup) {
 
-        $scope.lesson = entity;
-        $scope.subjectentrys = SubjectEntry.query();
-        $scope.teachers = Teacher.query();
-        $scope.studentgroups = StudentGroup.query();
-        $scope.load = function(id) {
-            Lesson.get({id : id}, function(result) {
-                $scope.lesson = result;
-            });
-        };
+            $scope.lesson = entity;
+            $scope.subjectentrys = SubjectEntry.query();
+            $scope.teachers = Teacher.query();
+            $scope.studentgroups = StudentGroup.query();
+            $scope.load = function (id) {
+                Lesson.get({id: id}, function (result) {
+                    $scope.lesson = result;
+                });
+            };
 
-        var onSaveSuccess = function (result) {
-            $scope.$emit('misApp:lessonUpdate', result);
-            $uibModalInstance.close(result);
-            $scope.isSaving = false;
-        };
+            var onSaveSuccess = function (result) {
+                $scope.$emit('misApp:lessonUpdate', result);
+                $uibModalInstance.close(result);
+                $scope.isSaving = false;
+            };
 
-        var onSaveError = function (result) {
-            $scope.isSaving = false;
-        };
+            var onSaveError = function (result) {
+                $scope.isSaving = false;
+            };
 
-        $scope.save = function () {
-            $scope.isSaving = true;
-            if ($scope.lesson.id != null) {
-                Lesson.update($scope.lesson, onSaveSuccess, onSaveError);
-            } else {
-                Lesson.save($scope.lesson, onSaveSuccess, onSaveError);
-            }
-        };
+            $scope.save = function () {
+                $scope.isSaving = true;
+                if ($scope.lesson.id != null) {
+                    Lesson.update($scope.lesson, onSaveSuccess, onSaveError);
+                } else {
+                    Lesson.save($scope.lesson, onSaveSuccess, onSaveError);
+                }
+            };
 
-        $scope.clear = function() {
-            $uibModalInstance.dismiss('cancel');
-        };
-        $scope.datePickerForStart = {};
+            $scope.clear = function () {
+                $uibModalInstance.dismiss('cancel');
+            };
 
-        $scope.datePickerForStart.status = {
-            opened: false
-        };
-
-        $scope.datePickerForStartOpen = function($event) {
-            $scope.datePickerForStart.status.opened = true;
-        };
-}]);
+        }]);
